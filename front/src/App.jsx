55d8226b1a0f7d3d471a70d6c2fa4ada71f518a0
Vann06 <cas23201@uvg.edu.gt>
2025-05-21 08:03:50 -0600

    HomeView wip

---
+++ front/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeView from './pages/HomeView';
+
+export default function App() {
+  return (
+    <div className="App">
+      <HomeView />
+    </div>
+  );
+}

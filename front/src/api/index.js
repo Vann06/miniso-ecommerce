d9ab64cbe05493332887484cd753519020e2bf8b
Vann06 <cas23201@uvg.edu.gt>
2025-05-27 20:39:36 -0600

    index update file

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:3001/api',
+  baseURL: process.env.REACT_API || 'http://localhost:3001/api',
 });
 
 export default api;

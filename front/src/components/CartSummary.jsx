94286546c2f9cdd79073a7a0686ac2a8a8604af2
Vann06 <cas23201@uvg.edu.gt>
2025-06-03 08:09:01 -0600

    addon of eslint

--- front/src/components/CartSummary.jsx
+++ front/src/components/CartSummary.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo} from 'react';
+
 import '../styles/cartSummary.css';
 
 export default function CartSummary({ cartItems, onClearCart, onCheckout }) {
